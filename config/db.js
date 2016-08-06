6dde11770fec7b0081359cbaa4d32675157190db
Juan Carlos <juancafe2@gmail.com>
2016-08-06 11:16:38 -0700

    Ready for heroku with jaws db

--- config/db.js
+++ config/db.js
@@ -1,23 +1,32 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.JAWSDB_URL ||{
   host: 'localhost', 
   user: 'root',
   password: 'admin',
   database: 'testdb' })
 connection.connect();
 
-// connection.query(`create table city values (
-//                 name varchar(50),
-//                 location varchar(50)
-//               );`, err => {
+connection.query(`create table users (id int not null auto_increment primary key,
+    username varchar(50),
+    password varchar(50)`, err => {
 
-
-//      console.log('err ', err  )
-//   connection.query('select * from city', (err, rows, fields) => {
-//      console.log('rows ', rows )
-//     connection.end()
-//   });
-// })
+    if(err) throw err;
+    else {
+      connection.query(`insert into users 
+        (username, password) 
+        VALUES ("juancafe2","admin").
+        ("user2","secret"),
+        ("user1","secret"),
+        ("root","admin"),` , err => {
+          if(err) throw err;
+        })
+    }
+    //  console.log('err ', err  )
+    // connection.query('select * from city', (err, rows, fields) => {
+    //  console.log('rows ', rows )
+    // connection.end()
+  });
+})
 
 module.exports = connection
